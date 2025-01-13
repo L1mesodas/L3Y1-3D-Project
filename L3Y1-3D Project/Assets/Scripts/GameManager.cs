ae728814020f06bcce09f931b059b2792b02bbeb
L1mesodas <goo23003167@heartofyorkshire.ac.uk>
2025-01-13 16:06:08 +0000

    Timer,collectables wip
    im so tired and i tried to add collectables

---
+++ L3Y1-3D Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float timer;
+
+    public float timeLimit;
+
+    public TMP_Text timerText;
+
+    [Header("Collectables")]
+    public int requiredCollectables;
+
+    public int currentCollectables;
+
+    public TMP_Text collectablesText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = timeLimit;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerText.text = timer.ToString("F2");
+        
+        if (timer <= 0)
+        {
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+        
+        
+    }
+    void Collectables()
+    {
+        if (currentCollectables == requiredCollectables)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+}
