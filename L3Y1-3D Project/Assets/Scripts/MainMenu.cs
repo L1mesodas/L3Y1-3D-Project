8bdab44c5a06b147a955fe7153cf18621fe3b63e
L1mesodas <goo23003167@heartofyorkshire.ac.uk>
2025-01-30 16:04:46 +0000

    mainmen

---
+++ L3Y1-3D Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas main;
+
+    public Canvas options;
+    // Start is called before the first frame update
+    void Start()
+    {
+        main.enabled = true;
+        options.enabled = false;
+    }
+
+    public void OnPlay()
+    {
+        
+    }
+    public void OnOptions()
+    {
+
+    }
+    public void OnExit()
+    {
+
+    }
+    public void OnPlay()
+}
